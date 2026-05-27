d52c11c1b64081052c48ae014e5f64cb67b9b764
Taís Caroline Enge <tais.enge@ska.com.br>
2026-05-26 22:59:25 -0300

    2023

---
+++ 2023/projetos23.js
@@ -0,0 +1,260 @@
+window.PROJETOS_DATA = {
+  ano: "2023",
+
+  projetos: [
+    {
+      id: "2023-t1-projeto-1",
+      turma: "t1",
+      titulo: "GaFer Maze",
+      alunos: "Eduarda Mainardi, Kalyany Santoro e Taís Enge",
+      thumb: "./imagens/thumbs/GaFerMazeT1.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaQuinta.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t1-projeto-2",
+      turma: "t1",
+      titulo: "Labirinto do Ratão",
+      alunos: "Rafael Petry e Kauan de Camargo",
+      thumb: "./imagens/thumbs/LabirintoDoRataoT1.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaQuinta.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t1-projeto-3",
+      turma: "t1",
+      titulo: "Maze game",
+      alunos: "Rafael Baron do Prado e Guilherme Schneider",
+      thumb: "./imagens/thumbs/MazeGameT1.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaQuinta.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t31-projeto-4",
+      turma: "t1",
+      titulo: "Calculadora",
+      alunos: "Raquel Karling Rodrigues",
+      thumb: "./imagens/thumbs/CalculadoraT1.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaQuinta.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t2-projeto-1",
+      turma: "t2",
+      titulo: "A aventura de Mario",
+      alunos: "Andressa, MIchele e Maria",
+      thumb: "./imagens/thumbs/AventuraDeMarioT2.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaM.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t2-projeto-2",
+      turma: "t2",
+      titulo: "Pacman",
+      alunos: "Eduardo e Daniel",
+      thumb: "./imagens/thumbs/PacmanT2.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaM.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t2-projeto-3",
+      turma: "t2",
+      titulo: "Mr BACK",
+      alunos: "Emerson e Éros",
+      thumb: "./imagens/thumbs/MrBackT2.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaM.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t2-projeto-4",
+      turma: "t2",
+      titulo: "Labirinto Lemon",
+      alunos: "Taís V. e Laura",
+      thumb: "./imagens/thumbs/LabirintoLemondT2.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaM.jpg",
+        legenda: "Turma 1 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t3-projeto-1",
+      turma: "t3",
+      titulo: "Os estudiosos",
+      alunos: "Andréia e Miguel",
+      thumb: "./imagens/thumbs/OsEstudiososT3.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaT.jpg",
+        legenda: "Turma 3 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t3-projeto-2",
+      turma: "t3",
+      titulo: "Grêmio no Mundial",
+      alunos: "Artur e Cleberson",
+      thumb: "./imagens/thumbs/GremioNoMundialT3.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaT.jpg",
+        legenda: "Turma 3 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t3-projeto-3",
+      turma: "t3",
+      titulo: "Chavirinto",
+      alunos: "Arthur e Samuel",
+      thumb: "./imagens/thumbs/ChavirintoT3.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaT.jpg",
+        legenda: "Turma 3 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t3-projeto-4",
+      turma: "t3",
+      titulo: "Calculadora Operacional PCO",
+      alunos: "Bruno e André",
+      thumb: "./imagens/thumbs/CalculadoraOperacionalPCOT3.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSextaT.jpg",
+        legenda: "Turma 3 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t4-projeto-1",
+      turma: "t4",
+      titulo: "Terrifying Phantom",
+      alunos: "Bruna e Débora",
+      thumb: "./imagens/thumbs/TerrifyingPhantomT4.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSab.jpg",
+        legenda: "Turma 4 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t4-projeto-2",
+      turma: "t4",
+      titulo: "Projeto Calculadora",
+      alunos: "Gabrieli e Vitória",
+      thumb: "./imagens/thumbs/projetocalculadoraT4.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSab.jpg",
+        legenda: "Turma 4 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t4-projeto-3",
+      turma: "t4",
+      titulo: "Labirinto",
+      alunos: "Maquelly e Caio",
+      thumb: "./imagens/thumbs/labirintoT4.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSab.jpg",
+        legenda: "Turma 4 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t4-projeto-4",
+      turma: "t4",
+      titulo: "Labirinto",
+      alunos: "Michel e Gustavo",
+      thumb: "./imagens/thumbs/LabirintoMT4.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSab.jpg",
+        legenda: "Turma 4 - 2023"
+      }
+    },
+
+    {
+      id: "2023-t4-projeto-5",
+      turma: "t4",
+      titulo: "Projeto Calculadora",
+      alunos: "Ricardo e Tainá",
+      thumb: "./imagens/thumbs/CalculadoraT4.png",
+      arquivo: "jogos/PREENCHER-PASTA-DO-JOGO/index.html",
+
+      galeria: {
+        tipo: "coletiva",
+        foto: "./imagens/turma/23TurmaSab.jpg",
+        legenda: "Turma 4 - 2023"
+      }
+    }
+  ]
+};
